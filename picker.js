4ce66004c9aef92f0b57a2805abb8ce20080bef2
David Tenni <davidctenni@gmail.com>
2023-02-18 21:26:20 +1100

    added: the main function is now working on ent

--- picker.js
+++ picker.js
@@ -1,16 +1,36 @@
 console.log('2')
 var x = document.getElementById('submit');
 console.log(x)
-let entStart = document.getElementById('ent-start-season');
-let entEnd = document.getElementById('ent-end-season');
+const entStart = document.getElementById('ent-start-season');
+const entEnd = document.getElementById('ent-end-season');
+const entCheck = document.getElementById('ent-check');
+let selectedEpisodes=[];
 
 
-
-
-function displayValues() {
-    document.getElementById("output").innerHTML = "Hello World" + entStart.value + entEnd.value;
+function getEntEpisode(){
+    starTrekEnterprise.forEach(episode => {
+    if (episode.season >= entStart.value && episode.season <= entEnd.value){
+        selectedEpisodes.push(episode);
+    }
+    });
+    console.log(selectedEpisodes);
 
 }
 
+function displayValues() {
+    document.getElementById("output").innerHTML = "Hello World" + entStart.value + entEnd.value + starTrekEnterprise[0].name;
+
+}
+
+function main(){
+    let includeEnt = entCheck.checked;
+    console.log(includeEnt);
+    if (includeEnt){
+        getEntEpisode();
+    };
+
+
+
+}
 const randomiseButton = document.getElementById("randomise");
-randomiseButton.addEventListener("click", displayValues);
+randomiseButton.addEventListener("click", main);
