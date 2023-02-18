44e922b3455a9b2f95802f383a2f2271253c299d
David Tenni <davidctenni@gmail.com>
2023-02-18 22:11:10 +1100

    added: star trek tng

--- picker.js
+++ picker.js
@@ -8,6 +8,10 @@ const tosStart = document.getElementById('tos-start-season');
 const tosEnd = document.getElementById('tos-end-season');
 const tosCheck = document.getElementById('tos-check');
 
+const tngStart = document.getElementById('tng-start-season');
+const tngEnd = document.getElementById('tng-end-season');
+const tngCheck = document.getElementById('tng-check');
+
 let selectedEpisodes=[];
 
 
@@ -30,7 +34,15 @@ function getTosEpisodes(){
     console.log(selectedEpisodes);
 
 }
+function getTngEpisodes(){
+    tngEpisodes.forEach(episode => {
+    if (episode.season >= tngStart.value && episode.season <= tngEnd.value){
+        selectedEpisodes.push(episode);
+    }
+    });
+    console.log(selectedEpisodes);
 
+}
 function displayValues(episode) {
     document.getElementById("output").innerHTML = `You should try watching: ${episode.show} Season ${episode.season} Episode ${episode.episode} ${episode.name} ` ;
 
@@ -39,6 +51,7 @@ function displayValues(episode) {
 function main(){
     let includeEnt = entCheck.checked;
     let includeTos = tosCheck.checked;
+    let includeTng = tngCheck.checked;
     console.log(includeEnt);
     if (includeEnt){
         getEntEpisodes();
@@ -46,7 +59,9 @@ function main(){
     if (includeTos){
         getTosEpisodes();
     };
-
+    if (includeTng){
+        getTngEpisodes();
+    };
     displayValues(selectedEpisodes[Math.floor(Math.random()*selectedEpisodes.length)])
     selectedEpisodes=[];
 
