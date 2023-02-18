99aab34494838eceeb721c0ee5b60c0a28fa0524
David Tenni <davidctenni@gmail.com>
2023-02-18 21:57:19 +1100

    enhanced: now working with ENT and TOS adding TNG

--- picker.js
+++ picker.js
@@ -1,14 +1,18 @@
-console.log('2')
-var x = document.getElementById('submit');
-console.log(x)
+
+const output = document.getElementById('output');
 const entStart = document.getElementById('ent-start-season');
 const entEnd = document.getElementById('ent-end-season');
 const entCheck = document.getElementById('ent-check');
+
+const tosStart = document.getElementById('tos-start-season');
+const tosEnd = document.getElementById('tos-end-season');
+const tosCheck = document.getElementById('tos-check');
+
 let selectedEpisodes=[];
 
 
-function getEntEpisode(){
-    starTrekEnterprise.forEach(episode => {
+function getEntEpisodes(){
+    entEpisodes.forEach(episode => {
     if (episode.season >= entStart.value && episode.season <= entEnd.value){
         selectedEpisodes.push(episode);
     }
@@ -17,19 +21,34 @@ function getEntEpisode(){
 
 }
 
-function displayValues() {
-    document.getElementById("output").innerHTML = "Hello World" + entStart.value + entEnd.value + starTrekEnterprise[0].name;
+function getTosEpisodes(){
+    tosEpisodes.forEach(episode => {
+    if (episode.season >= tosStart.value && episode.season <= tosEnd.value){
+        selectedEpisodes.push(episode);
+    }
+    });
+    console.log(selectedEpisodes);
+
+}
+
+function displayValues(episode) {
+    document.getElementById("output").innerHTML = `You should try watching: ${episode.show} Season ${episode.season} Episode ${episode.episode} ${episode.name} ` ;
 
 }
 
 function main(){
     let includeEnt = entCheck.checked;
+    let includeTos = tosCheck.checked;
     console.log(includeEnt);
     if (includeEnt){
-        getEntEpisode();
+        getEntEpisodes();
+    };
+    if (includeTos){
+        getTosEpisodes();
     };
 
-
+    displayValues(selectedEpisodes[Math.floor(Math.random()*selectedEpisodes.length)])
+    selectedEpisodes=[];
 
 }
 const randomiseButton = document.getElementById("randomise");
